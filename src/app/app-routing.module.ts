19c116a000a3547ad9b9807df556dad480e49d33
Hassaan Nasir <zincoxide38@gmail.com>
2024-01-17 00:49:39 +0500

    I created add employee form to add details of employee and then show the details in show employee component through input output decoraters

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { CounterAppComponent } from './counter-app/counter-app.component';
 import { FormComponent } from './form/form.component';
 import { FormsComponent } from './forms/forms.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { AddEmpComponent } from './task4/add-emp/add-emp.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'forms', pathMatch: 'full' },
 
   { path: 'counter', component: CounterAppComponent },
+  { path: 'add-emp', component: AddEmpComponent },
   {
     path: 'form',
     component: FormComponent,
